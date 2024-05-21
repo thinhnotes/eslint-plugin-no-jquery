79ca668f58463e5addb7fd2e735fe5cc9a0af3dc
Thinh Nguyen <thinh.nguyenhung@nashtechglobal.com>
2024-05-21 17:36:28 +0700

    fix isues for click

--- src/rules/no-event-shorthand-click.js
+++ src/rules/no-event-shorthand-click.js
@@ -1,12 +1,57 @@
 'use strict';
 const utils = require( '../utils.js' );
+
+const ajaxEvents = [
+	'ajaxComplete',
+	'ajaxError',
+	'ajaxSend',
+	'ajaxStart',
+	'ajaxStop',
+	'ajaxSuccess'
+];
+
+const events = [
+	// Browser
+	'error',
+	// Can't disallow 'load' as it conflicts with Ajax load.
+	// Use no-load-shorthand rule instead.
+	// TODO: Share the logic of no-load-shorthand with this rule.
+	'resize',
+	'scroll',
+	'unload',
+	// Form
+	'blur',
+	'change',
+	'focus',
+	'focusin',
+	'focusout',
+	'select',
+	'submit',
+	// Keyboard
+	'keydown',
+	'keypress',
+	'keyup',
+	// Mouse
+	'click',
+	'contextmenu',
+	'dblclick',
+	'hover',
+	'mousedown',
+	'mouseenter',
+	'mouseleave',
+	'mousemove',
+	'mouseout',
+	'mouseover',
+	'mouseup'
+].concat( ajaxEvents );
+
 const rule = {
 	// eslint-disable-next-line eslint-plugin/require-meta-type
 	meta: {
 		fixable: 'code',
 		schema: [],
 		messages: {
-			clickErrorMgs: 'Prefer .on or .trigger to .click'
+			clickErrorMgs: 'Prefer .on or .trigger to .{{event}}'
 		}
 	},
 	create( context ) {
@@ -17,13 +62,17 @@ const rule = {
 					isJqueryVar = isJquery( node );
 				}
 				console.log( isJqueryVar );
-				if ( isJqueryVar && node.callee.type === 'MemberExpression' && node.callee.property.name === 'click' ) {
+				if ( isJqueryVar && node.callee.type === 'MemberExpression' && events.includes( node.callee.property.name ) ) {
 					context.report( {
 						node,
 						messageId: 'clickErrorMgs',
 						fixable: 'code',
+						data: {
+							event: node.callee.property.name
+						},
 						fix: function ( fixer ) {
-							return utils.eventShorthandFixer( node, context, fixer, 'click' );
+							// eslint-disable-next-line max-len
+							return utils.eventShorthandFixer( node, context, fixer, node.callee.property.name );
 						}
 					} );
 				}
