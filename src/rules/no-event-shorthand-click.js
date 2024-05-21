cf123bac04a05a6755da138477d002b251224c8d
Thinh Nguyen <thinh.nguyenhung@nashtechglobal.com>
2024-05-21 14:47:21 +0700

    update code

---
+++ src/rules/no-event-shorthand-click.js
@@ -0,0 +1,31 @@
+'use strict';
+const utils = require( '../utils.js' );
+const rule = {
+	// eslint-disable-next-line eslint-plugin/require-meta-type
+	meta: {
+		fixable: 'code',
+		schema: [],
+		messages: {
+			avoidName: 'Prefer .on or .trigger to .click'
+		}
+	},
+	create( context ) {
+		return {
+			'CallExpression:exit': function ( node ) {
+				if ( node.callee.type === 'MemberExpression' && node.callee.property.name === 'click' ) {
+					console.log( 'node', node.callee.object );
+					context.report( {
+						node,
+						messageId: 'avoidName',
+						fixable: 'code',
+						fix: function ( fixer ) {
+							return utils.eventShorthandFixer( node, context, fixer, 'click' );
+						}
+					} );
+				}
+			}
+		};
+	}
+};
+
+module.exports = rule;
---
+++ tests/rules/no-event-shorthand-click.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const rule = require( '../../src/rules/no-event-shorthand-click' );
+const RuleTester = require( '../../tools/rule-tester' );
+const error = 'Prefer .on or .trigger to .click';
+
+const ruleTester = new RuleTester();
+
+ruleTester.run( 'no-event-shorthand-click', rule, {
+	valid: [  ],
+	invalid: [
+		{
+			code: '$("div")[0].click()',
+			errors: [ error ],
+			output: '$("div")[0].trigger("click")',
+			docgen: false
+		},
+		{
+			code: 'var fileUploadButton = $("#demo"); fileUploadButton.click()',
+			errors: [ error ],
+			output: 'var fileUploadButton = $("#demo"); fileUploadButton.trigger("click")',
+			docgen: false
+		},
+		{
+			code: '$(".popupImage:last")[0].click()',
+			errors: [ error ],
+			output: '$(".popupImage:last")[0].trigger("click")',
+			docgen: false
+		}
+	]
+} );
